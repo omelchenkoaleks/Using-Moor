af19cd69eaef31fffdedd879faa19fa2cf22245f
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-01-20 07:08:50 +0200

    add dependencies

---
+++ lib/data/moor/moor_db.dart
@@ -0,0 +1,20 @@
+import 'package:moor_flutter/moor_flutter.dart';
+import '../models/models.dart';
+
+part 'moor_db.g.dart';
+
+// TODO: Add MoorRecipe table definition here
+
+// TODO: Add MoorIngredient table definition here
+
+// TODO: Add @UseMoor() and RecipeDatabase() here
+
+// TODO: Add RecipeDao here
+
+// TODO: Add IngredientDao
+
+// TODO: Add moorRecipeToRecipe here
+
+// TODO: Add MoorRecipeData here
+
+// TODO: Add moorIngredientToIngredient and MoorIngredientCompanion here
