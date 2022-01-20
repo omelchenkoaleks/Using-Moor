a70247c17cb8eae170649e650526ab875831382f
omelchenkoaleks <omelchenkoaleks@gmail.com>
2022-01-20 07:15:14 +0200

    creating tables

--- lib/data/moor/moor_db.dart
+++ lib/data/moor/moor_db.dart
@@ -3,7 +3,25 @@ import '../models/models.dart';
 
 part 'moor_db.g.dart';
 
-// TODO: Add MoorRecipe table definition here
+// Create a class named MoorRecipe that extends Table.
+class MoorRecipe extends Table {
+  // You want a column named id that is an integer. autoIncrement() automatically creates the IDs for you.
+  IntColumn get id => integer().autoIncrement()();
+
+  // Create a label column made up of text.
+  TextColumn get label => text()();
+
+  TextColumn get image => text()();
+
+  TextColumn get url => text()();
+
+  RealColumn get calories => real()();
+
+  RealColumn get totalWeight => real()();
+
+  RealColumn get totalTime => real()();
+}
+
 
 // TODO: Add MoorIngredient table definition here
 
